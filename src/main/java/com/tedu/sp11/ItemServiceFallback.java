43417879498abd10f6041e9cd2ba44bb869676f5
UID <UID@DESKTOP-DACJOCN>
2019-06-28 14:28:08 +0800

    dfsf

--- src/main/java/com/tedu/sp11/ItemServiceFallback.java
+++ src/main/java/com/tedu/sp11/ItemServiceFallback.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ItemServiceFallback implements FallbackProvider {
 	@Override
 	public String getRoute() {
-		//d11
+		//d111
 		//542lalala
 	    //当执行item-service失败，
 	    //应用当前这个降级类
